221f74e5e8b64b17c1305576f2d5962e3384c22a
Ekaterina <67323470+Babaranka@users.noreply.github.com>
2020-06-23 21:45:02 +0300

    init

---
+++ src/pages/Example.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1>Example page</h1>
+        <input type="text" v-model="message"/>
+        <button class="btn btnPrimary" @click="setMessage">Change message</button>
+        <br>
+        <br>
+        <p>{{getMessage}}</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      message: null
+    };
+  },
+  // created () { //способ получения данных из state store меняя date
+  //   this.message = this.$store.getters.getMessage
+  // },
+  computed: {
+    getMessage() {
+      return this.$store.getters.getMessage
+    }
+  },
+  methods: {
+    setMessage() {
+      this.$store.dispatch("setMessage", this.message)
+    }
+  }
+};
+</script>
