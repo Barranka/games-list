dcc992aad27fad17086adb6aab07ff4f0a47d928
katya <cat123_93@mail.ru>
2020-11-12 12:58:15 +0300

    first commit

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="container container-content">
-      <ul class="menu-list">
-        <li class="menu-item"  @click="makeActive(i)" :class="{active: link.isActive }" v-for="(link, i) in links" :key="link.title">
+      <ul class="menu-wrap">
+        <li class="menu-item" :class="{active: link.isActive }" v-for="(link, i) in links" :key="link.title">
           <router-link class="menu-link" :title="link.title" :to="link.url">
             <img v-if="link.icon" class="menu-link__icon" :src="getImgUrl(`./menu/${link.icon}.svg`)" alt="вид игр">
             <div class="menu-link__title">{{link.title}}</div>
@@ -20,13 +20,13 @@ export default {
   data () {
     return {
       links: [
-        {title: 'All GAMES', url: '/', icon: 'star', isActive: true},
-        {title: 'TOP GAMES', url: '/top-games', icon: 'top', isActive: null},
-        {title: 'LIVE CASINO', url: '/live-casino', icon: 'live', isActive: null},
-        {title: 'SLOTS GAMES', url: '/slots-games', icon: 'seven', isActive: null},
-        {title: 'ROULETTE', url: '/roulette', icon: 'roulette', isActive: null},
-        {title: 'TABLE GAMES', url: '/table-games', icon: 'table', isActive: null},
-        {title: 'CARD GAMES', url: '/card-games', icon: 'cards', isActive: null}
+        {title: 'All GAMES', url: '/', icon: 'star'},
+        {title: 'TOP GAMES', url: '/top-games', icon: 'top'},
+        {title: 'LIVE CASINO', url: '/live-casino', icon: 'live'},
+        {title: 'SLOTS GAMES', url: '/slots-games', icon: 'seven'},
+        {title: 'ROULETTE', url: '/roulette', icon: 'roulette'},
+        {title: 'TABLE GAMES', url: '/table-games', icon: 'table'},
+        {title: 'CARD GAMES', url: '/card-games', icon: 'cards'}
       ],
     }
   },
@@ -34,12 +34,6 @@ export default {
     getImgUrl(fileName) {
       const images = require.context('@/assets/images/', true, /\.(png|jpe?g|svg)$/);
       return images(fileName);
-    },
-    makeActive(i){
-      this.links.forEach((item, index)=>{
-        item.isActive = false;
-        if (index == i) item.isActive = true;
-      })
     }
   }
 };
